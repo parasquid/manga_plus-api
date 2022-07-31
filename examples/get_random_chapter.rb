e0006e63fb54b5362bee562ebac5544c17e3e67c
parasquid <tristan.gomez@gmail.com>
2022-07-31 17:57:44 +0800

    fix rubocop warnings

--- examples/get_random_chapter.rb
+++ examples/get_random_chapter.rb
@@ -9,8 +9,8 @@ include MangaPlus::Api
 
 all_titles_view = AllTitlesView.new
 title_id = all_titles_view.call[:titles]
-  .select { |title| title[:language] == :ENGLISH }
-  .sample[:titleId]
+                          .select { |title| title[:language] == :ENGLISH }
+                          .sample[:titleId]
 
 title_detail_view = TitleDetailView.new(title_id)
 chapter_id = title_detail_view.call[:firstChapterList].first[:chapterId]
@@ -19,19 +19,21 @@ manga_viewer = MangaViewer.new(chapter_id)
 manga = manga_viewer.call
 
 folder = "#{manga[:titleName]} #{manga[:chapterName]} - #{chapter_id}"
-Dir.mkdir folder unless Dir.exists?(folder); Dir.chdir folder
+Dir.mkdir folder unless Dir.exist?(folder)
+Dir.chdir folder
 
 manga[:pages].compact.each_with_index do |page, index|
   next unless page[:mangaPage]
 
-  filename = "#{(index + 1).to_s.rjust(3, '0')}"
+  filename = (index + 1).to_s.rjust(3, '0')
   key = page[:mangaPage][:encryptionKey]
   type = page[:mangaPage][:type]
 
   image = HTTParty.get(page[:mangaPage][:imageUrl]).body
   decoded_image = Utils.decode(image, encryption_key: key)
 
-  File.open(filename + " [#{type}]" + ".jpg", 'wb') do |file|
-    file.write(decoded_image.pack("C*"))
+  fullname = "#{filename} [#{type}].jpg"
+  File.open(fullname, 'wb') do |file|
+    file.write(decoded_image.pack('C*'))
   end
 end
